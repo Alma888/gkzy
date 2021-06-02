212520f681c32d002638bada8f195ef7391fc41a
Alma <2484094776qq.com>
2021-06-02 23:04:29 +0800

    678

--- src/main/java/com/lj/gkzy/common/constant/ObtainingScoreDataConstant.java
+++ src/main/java/com/lj/gkzy/common/constant/ObtainingScoreDataConstant.java
@@ -3,14 +3,25 @@ package com.lj.gkzy.common.constant;
 /**
  * 获取学校录取情况信息的常量类
  *
- * @author liujing
+ * @author xiaoxi
  * @created 2021/4/3
  */
 public class ObtainingScoreDataConstant {
     /**
      * 西安科技大学录取网页首页地址
      */
-    public static final String XUST_HOME_PAGE_URL = "https://zs.xust.edu.cn/fsxrk.jsp?urltype=tree.TreeTempUrl&wbtreeid=1043";
+    public static final String XUST_HOME_PAGE_URL =
+            "https://zs.xust.edu.cn/fsxrk.jsp?urltype=tree.TreeTempUrl&wbtreeid=1043";
+    /**
+     * 西北工业大学录取网页首页地址
+     */
+    public static final String Factory_HOME_PAGE_URL=
+            "https://zsb.nwpu.edu.cn/static/front/nwpu/basic/html_web/lnfs.html";
+    /**
+     * 西北大学录取网页首页地址
+     */
+    public static final String XiBei_HOME_PAGE_URL=
+            "https://zhinengdayi.com/page/detail/FCHTWR/2464/6548";
 
     /**
      * 西安科技大学录取数据请求地址
@@ -21,10 +32,10 @@ public class ObtainingScoreDataConstant {
      * 西安科技大学名称
      */
     public static final String XUST_SCHOOL_NAME = "西安科技大学";
-    /**
-     * 西北工业大学录取网页首页地址
-     */
-    public static final String Factory_HOME_PAGE_URL="https://zsb.nwpu.edu.cn/static/front/nwpu/basic/html_web/lnfs.html";
+//    /**
+//     * 西北工业大学录取网页首页地址
+//     */
+//    public static final String Factory_HOME_PAGE_URL="https://zsb.nwpu.edu.cn/static/front/nwpu/basic/html_web/lnfs.html";
 
     /**
      * 西北工业大学录取数据请求地址
@@ -35,10 +46,10 @@ public class ObtainingScoreDataConstant {
      * 西北工业大学名称
      */
     public static final String Factory_SCHOOL_NAME = "西北工业大学";
-    /**
-     * 西北大学录取网页首页地址
-     */
-    public static final String XiBei_HOME_PAGE_URL="https://zhinengdayi.com/page/detail/FCHTWR/2464/6548";
+//    /**
+//     * 西北大学录取网页首页地址
+//     */
+//    public static final String XiBei_HOME_PAGE_URL="https://zhinengdayi.com/page/detail/FCHTWR/2464/6548";
 
     /**
      * 西北大学录取数据请求地址
---
+++ src/main/java/com/lj/gkzy/domain/model/AllScoreModel.java
@@ -0,0 +1,4 @@
+package com.lj.gkzy.domain.model;
+
+public class allScoreModel {
+}
